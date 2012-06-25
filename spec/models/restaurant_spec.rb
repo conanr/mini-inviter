9208fd8d388f5f2b8dc7dab72dbecb546819c6e0
Conan Rimmer <conan@livingsocial.com>
2012-06-25 11:19:41 -0400

    do not import the same LS restaurant twice

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -12,6 +12,11 @@ describe Restaurant do
     restaurant.address.should == "1445 New York Ave. NW, Washington, DC 20005"
   end
 
+  it "has a LivingSocial ID" do
+    restaurant = Restaurant.create(name: "Hello", address: "1445 New York Ave. NW, Washington, DC 20005", ls_id: "2342")
+    restaurant.ls_id.should == "2342"
+  end
+
   it "requires a name" do
     restaurant = Restaurant.create()
     restaurant.valid?.should be_false
@@ -22,12 +27,27 @@ describe Restaurant do
     restaurant.valid?.should be_false
   end
   
+  it "requires a unique LivingSocial ID" do
+    Restaurant.create(name: "Hello", address: "1445 New York Ave. NW, Washington, DC 20005", ls_id: "123")
+    restaurant = Restaurant.create(name: "Goodbye", address: "123 11th St. NW, Washington, DC 20001", ls_id: "123")
+    restaurant.valid?.should be_false
+  end
+  
   describe ".import_from_ls_page" do
     it "creates a new restaurant from a scraped LS page" do
       scraped_page = Nokogiri::HTML(File.open('spec/support/assets/ls_toad_show_dc_indian_delight.html'))
       restaurant = Restaurant.import_from_ls_page(scraped_page)
-      restaurant.name.should == "Indian Delight"
+      restaurant.name.should    == "Indian Delight"
       restaurant.address.should == "1100 Pennsylvania Avenue NW Washington, DC 20004"
+      restaurant.ls_id.should   == "7356"
+    end
+    
+    it "does not import the same restaurant twice" do
+      scraped_page = Nokogiri::HTML(File.open('spec/support/assets/ls_toad_show_dc_indian_delight.html'))
+      expect { 
+        Restaurant.import_from_ls_page(scraped_page)
+        Restaurant.import_from_ls_page(scraped_page)
+      }.to change { Restaurant.count }.by(1)
     end
   end
 end
