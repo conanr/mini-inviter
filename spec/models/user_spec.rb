7970eb595059ab6842821c41e0d380f10220d766
Conan Rimmer <conan@livingsocial.com>
2012-06-25 17:08:55 -0400

    make sure User.create_from_oauth_hash is idempotent

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,13 @@ describe User do
       user.email.should         == "dcbrewman@example.com"
       user.foursquare_id.should == "1234"
     end
+    
+    it "does not create two user records for the same foursquare user" do
+      oauth_hash = Hashie::Mash.new(JSON.parse(File.open("spec/support/assets/foursquare_oauth_hash_test_account.txt").read))
+      expect {
+        User.create_from_oauth_hash oauth_hash
+        User.create_from_oauth_hash oauth_hash
+      }.to change { User.count }.by 1
+    end
   end
 end
