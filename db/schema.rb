e090197052c205e1a37b226c7262787781ca44e2
Conan Rimmer <conan@livingsocial.com>
2012-06-26 11:11:34 -0400

    add start time and duration in minutes to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120626125631) do
+ActiveRecord::Schema.define(:version => 20120626150539) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(:version => 20120626125631) do
     t.string   "zip_code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.datetime "start_time"
+    t.integer  "duration"
   end
 
   create_table "restaurants", :force => true do |t|
