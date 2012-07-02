b9b180d78808336995235ce2a9e18177a3d309cd
Conan Rimmer <conan@livingsocial.com>
2012-07-02 09:40:25 -0400

    try again

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module Inviter
@@ -64,5 +64,8 @@ module Inviter
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # turn off for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
