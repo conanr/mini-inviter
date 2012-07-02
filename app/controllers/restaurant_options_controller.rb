f224dc6d6b659f190c190726eedafbb6aa727a52
Conan Rimmer <conan@livingsocial.com>
2012-07-01 21:16:24 -0400

    fix reek error

--- app/controllers/restaurant_options_controller.rb
+++ app/controllers/restaurant_options_controller.rb
@@ -1,15 +1,16 @@
 class RestaurantOptionsController < ApplicationController
   before_filter :authenticate_user
-  
+
   def new
     @event = Event.find params[:event_id]
     @nearby_restaurants = @event.nearby_restaurants
   end
-  
+
   def create
     @event = Event.find params[:event_id]
-    params[:restaurant].keys.each do |r| 
-      @event.restaurant_options << RestaurantOption.create(restaurant_id: r)
+    params[:restaurant].keys.each do |restaurant_id| 
+      option = RestaurantOption.create(restaurant_id: restaurant_id)
+      @event.restaurant_options << option
     end
     redirect_to new_event_invite_path @event
   end
