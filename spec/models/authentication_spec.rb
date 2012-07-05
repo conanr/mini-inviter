9260dfc0acf4385c458bc7e2f3817d0030864804
Conan Rimmer <conan@livingsocial.com>
2012-07-05 12:53:22 -0400

    clean up authentication unit tests

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -5,42 +5,48 @@ describe Authentication do
                               uid:      "1445",
                               token:    "3NH22I530TIAIY5CXWPMUD44LT0D2FEDY0IBX0NY2QNE1SNL"  } }
   
-  it "requires a non-blank provider" do
-    auth_attributes[:provider] = nil
-    auth = Authentication.create auth_attributes
-    auth.valid?.should be_false
-
-    auth.update_attribute :provider, " "
-    auth.valid?.should be_false
+  context "with invalid data" do
+    it "rejects blank providers" do
+      [" ", nil].each do |invalid_provider|
+        auth_attributes[:provider] = invalid_provider
+        auth = Authentication.create auth_attributes
+        auth.valid?.should be_false
+      end
+    end
     
-    auth.update_attribute :provider, "foursquare"
-    auth.valid?.should be_true
-    auth.provider.should == "foursquare"
+    it "rejects blank uids" do
+      [" ", nil].each do |invalid_uid|
+        auth_attributes[:uid] = invalid_uid
+        auth = Authentication.create auth_attributes
+        auth.valid?.should be_false
+      end
+    end
   end
-  
-  it "requires a non-blank uid" do
-    auth_attributes[:uid] = nil
-    auth = Authentication.create auth_attributes
-    auth.valid?.should be_false
 
-    auth.update_attribute :uid, " "
-    auth.valid?.should be_false
+  context "with valid data" do
+    it "accepts a non-blank provider" do
+      auth = Authentication.create auth_attributes
+      auth.valid?.should be_true
+      auth.provider.should == auth_attributes[:provider]
+    end
     
-    auth.update_attribute :uid, "1234"
-    auth.valid?.should be_true
-    auth.uid.should == "1234"
-  end
-  
-  it "does not create duplicate authentications for an existing provider/uid pair" do
-    expect {
-      Authentication.create auth_attributes
-      Authentication.create auth_attributes
-    }.to change { Authentication.count }.by 1
-  end
-  
-  it "may have a token" do
-    auth = Authentication.create auth_attributes
-    auth.token.should == "3NH22I530TIAIY5CXWPMUD44LT0D2FEDY0IBX0NY2QNE1SNL"
+    it "accepts a non-blank uid" do
+      auth = Authentication.create auth_attributes
+      auth.valid?.should be_true
+      auth.uid.should == auth_attributes[:uid]
+    end
+    
+    it "does not create duplicate authentications for an existing provider/uid pair" do
+      expect {
+        Authentication.create auth_attributes
+        Authentication.create auth_attributes
+      }.to change { Authentication.count }.by 1
+    end
+    
+    it "may have a token" do
+      auth = Authentication.create auth_attributes
+      auth.token.should == auth_attributes[:token]
+    end
   end
   
   describe '.from_oauth_hash' do
@@ -49,19 +55,18 @@ describe Authentication do
     
     it 'creates a new authentication if none already exist for the user' do
       auth = Authentication.from_oauth_hash(user, oauth_hash)
-      auth.provider.should  == "foursquare"
-      auth.uid.should       == "1234"
-      auth.token.should     == "3NH22I530TIAIY5CXWPMUD44LT0D2FEDY0IBX0NY2QNE1SNL"
+      auth.provider.should  == oauth_hash["provider"]
+      auth.uid.should       == oauth_hash["uid"]
+      auth.token.should     == oauth_hash["credentials"]["token"]
       user.authentications.last.should == auth
     end
     
     it 'returns an authentication if one already exist for the user' do
       Authentication.from_oauth_hash(user, oauth_hash)
       auth = Authentication.from_oauth_hash(user, oauth_hash)
-      auth.provider.should  == "foursquare"
-      auth.uid.should       == "1234"
-      auth.token.should     == "3NH22I530TIAIY5CXWPMUD44LT0D2FEDY0IBX0NY2QNE1SNL"
-      user.authentications.last.should == auth
+      auth.provider.should  == oauth_hash["provider"]
+      auth.uid.should       == oauth_hash["uid"]
+      auth.token.should     == oauth_hash["credentials"]["token"]
     end
   end
 end
