61b2dd7ccd329e83326f6a7a8baf1f60737d906b
Conan Rimmer <conan@livingsocial.com>
2012-07-05 15:05:46 -0400

    clean up contact unit tests

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -7,77 +7,70 @@ describe Address do
                            state:      "DC",
                            zip_code:   "20005" } }
   
-  
-  it 'requires a non-blank primary street address' do
-    event_address[:street1] = nil
-    address = Address.create event_address
-    address.valid?.should be_false
+  context "with invalid data" do
+    it 'rejects blank primary street addresses' do
+      ["    ", nil].each do |invalid_street1|
+        event_address[:street1] = invalid_street1
+        address = Address.create event_address
+        address.valid?.should be_false
+      end
+    end
     
-    address.update_attribute :street1, "    "
-    address.valid?.should be_false
-  
-    address.update_attribute :street1, "1445 New York Ave. NW"
-    address.valid?.should be_true
-    address.street1.should == "1445 New York Ave. NW"
+    it 'rejects blank city values' do
+      ["    ", nil].each do |invalid_city|
+        event_address[:city] = invalid_city
+        address = Address.create event_address
+        address.valid?.should be_false
+      end
+    end
+    
+    it 'rejects invalid state abbreviations' do
+      ["    ", nil, "ZZ"].each do |invalid_city|
+        event_address[:state] = nil
+        address = Address.create event_address
+        address.valid?.should be_false
+      end
+    end
+    
+    it 'rejects invalid zip codes' do
+      ["  ", nil, "2000", "ABCDE", "20005-123"].each do |invalid_zip|
+        event_address[:zip_code] = invalid_zip
+        address = Address.create event_address
+        address.valid?.should be_false
+      end
+    end
   end
   
-  it 'may have a secondary street address' do
-    address = Address.create event_address
-    address.street2.should == "Suite 201"
-  end
-  
-  it 'requires a non-blank city' do
-    event_address[:city] = nil
-    address = Address.create event_address
-    address.valid?.should be_false
+  context "with valid data" do
+    let!(:address)   { Address.create event_address }
     
-    address.update_attribute :city, "    "
-    address.valid?.should be_false
-  
-    address.update_attribute :city, "Washington"
-    address.valid?.should be_true
-    address.city.should == "Washington"
-  end
-  
-  it 'requires a valid state abbreviation' do
-    event_address[:state] = nil
-    address = Address.create event_address
-    address.valid?.should be_false
+    it 'accepts a non-blank primary street address' do
+      address.valid?.should be_true
+      address.street1.should == event_address[:street1]
+    end
     
-    address.update_attribute :state, "    "
-    address.valid?.should be_false
+    it 'may have a secondary street address' do
+      address.street2.should == "Suite 201"
+    end
   
-    address.update_attribute :state, "ZZ"
-    address.valid?.should be_false
-    
-    address.update_attribute :state, "DC"
-    address.valid?.should be_true
-    address.state.should == "DC"
-  end
+    it 'accepts a non-blank city' do
+      address.valid?.should be_true
+      address.city.should == "Washington"
+    end
   
-  it 'requires a valid zip code' do
-    event_address[:zip_code] = nil
-    address = Address.create event_address
-    address.valid?.should be_false
+    it 'accepts a valid state abbreviation' do
+      address.valid?.should be_true
+      address.state.should == "DC"
+    end
     
-    address.update_attribute :zip_code, "    "
-    address.valid?.should be_false
-  
-    address.update_attribute :zip_code, "ABCDE"
-    address.valid?.should be_false
-  
-    address.update_attribute :zip_code, "2000"
-    address.valid?.should be_false
-    
-    address.update_attribute :zip_code, "20005"
-    address.valid?.should be_true
-    
-    address.update_attribute :zip_code, "20005-123"
-    address.valid?.should be_false
-    
-    address.update_attribute :zip_code, "20005-1234"
-    address.valid?.should be_true
-    address.zip_code.should == "20005-1234"
+    it 'accepts a valid zip code' do
+      ["20005", "20005-1234"].each do |valid_zip|
+        event_address[:zip_code] = valid_zip
+        address = Address.create event_address
+        address.valid?.should be_true
+        address.zip_code.should == valid_zip
+      end
+    end
   end
   
   describe '#full_address' do
